1c884d49c75341907f0aa88aa24f1d523a9ebfb2
poseidoncarrot <matthew8888hh8888@hotmail.com>
2026-04-02 02:23:18 -0700

    Implemented UI/UX
    Implemented UI/UX for the initial MVP.

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { projectId, publicAnonKey } from '../../../utils/supabase/info';
+import { projectId, publicAnonKey } from '/utils/supabase/info.tsx';
 
 const supabaseUrl = `https://${projectId}.supabase.co`;
 export const supabase = createClient(supabaseUrl, publicAnonKey);
